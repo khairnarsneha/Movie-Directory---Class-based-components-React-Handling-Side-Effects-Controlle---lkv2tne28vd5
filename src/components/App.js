bd44c4a58071c0f962b648d28ead2ac43e48c68d
Sneha Hari Khairnar <109480526+khairnarsneha@users.noreply.github.com>
2023-02-25 21:32:31 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,16 +1,18 @@
 import React from "react";
 import "../styles/App.css";
+import MovieProvider from "./MovieContext";
 import Movie from "./Movie";
 import MoviesList from "./MoviesList";
 
 const App = () => {
-  
   return (
-    <div id="main">
-      <h1>Movie List</h1>
-      <MoviesList />
-      <Movie />
-    </div>
+    <MovieProvider>
+      <div id="main">
+        <h1>Movie List</h1>
+        <MoviesList />
+        <Movie />
+      </div>
+    </MovieProvider>
   );
 };
 
